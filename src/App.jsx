bd756a708e69f9ca718f9323a61a3e67eec830c1
Samad Aamir <samadaamir3@gmail.com>
2026-04-26 11:37:36 +0500

    counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num, setnum] = useState(0)
+
+  function increase(){
+    setnum(num+1)
+  }
+
+    function decrease(){
+    setnum(num-1)
+  }
+    function reset(){
+    setnum(0)
+  }
+
+
+
   return (
-    <div>App</div>
+    <div className='main'>
+      <h1>{num}</h1>
+      <div className='btn'>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={reset}>Reset</button>
+      </div>
+    </div>
   )
 }
 
